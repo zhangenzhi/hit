d46212805a5249e1ee0c83a542992267026a461a
Enzhi Zhang <zhangenzhi657@gmail.com>
2025-12-16 15:59:39 +0900

    test bacth mode.

--- script/dit-b_2_IN1K_latent_rc10.sh
+++ script/dit-b_2_IN1K_latent_rc10.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 #------ qsub option --------#
 #PBS -q lg
-#PBS -l select=2:ngpus=4
+#PBS -l select=2:ngpus=4:mpiprocs=4
 #PBS -l walltime=8:00:00
 #PBS -W group_list=c30636
 #PBS -j oe
 
-# # >>> conda initialize >>>
-# # !! Contents within this block are managed by 'conda init' !!
-# __conda_setup="$('/home/c30746/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-# if [ $? -eq 0 ]; then
-#     eval "$__conda_setup"
-# else
-#     if [ -f "/home/c30746/miniconda3/etc/profile.d/conda.sh" ]; then
-#         . "/home/c30746/miniconda3/etc/profile.d/conda.sh"
-#     else
-#         export PATH="/home/c30746/miniconda3/bin:$PATH"
-#     fi
-# fi
-# unset __conda_setup
-# # <<< conda initialize <<<
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/c30746/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/c30746/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/c30746/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/c30746/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
 
 cd /work/c30636/hit
-conda init
 conda activate hde
 
 torchrun --nnodes=2 --nproc_per_node=4 ./main.py --config ./configs/dit-xl_IN1K.yaml
