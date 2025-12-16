61a81471e6f8854fdb73d84fe259e9214299a1d8
Enzhi Zhang <zhangenzhi657@gmail.com>
2025-12-16 15:33:56 +0900

    test bacth mode.

--- script/dit-b_2_IN1K_latent_rc10.sh
+++ script/dit-b_2_IN1K_latent_rc10.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #------ qsub option --------#
-#PBS -q sg
-#PBS -l select=2:ngpus=8
-#PBS -l walltime=30:00
+#PBS -q lg
+#PBS -l select=2:ngpus=4
+#PBS -l walltime=8:00:00
 #PBS -W group_list=c30636
 #PBS -j oe
 
 
 cd /work/c30636/hit
+conda activate hde
 
-torchrun --nnodes=2 --nproc_per_node=4 ./main.py --config ./configs/dit-b_IN1K.yaml
+torchrun --nnodes=2 --nproc_per_node=4 ./main.py --config ./configs/dit-xl_IN1K.yaml
